03ab1d215ef999a6038bd31b7570a6013d29293f
Paul Tran <rsluan.github@gmail.com>
2022-06-05 20:28:11 +0700

    finished footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,156 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+import "./footer.css";
+
+const MY__ACOUNT = [
+  {
+    display: "Author Profile",
+    url: "/seller-profile",
+  },
+  {
+    display: "Create Item",
+    url: "/create",
+  },
+  {
+    display: "Collection",
+    url: "/market",
+  },
+  {
+    display: "Edit Profile",
+    url: "/edit-profile",
+  },
+];
+
+const RESOURCES = [
+  {
+    display: "Help Center",
+    url: "#",
+  },
+  {
+    display: "Partner",
+    url: "#",
+  },
+  {
+    display: "Community",
+    url: "#",
+  },
+  {
+    display: "Activity",
+    url: "#",
+  },
+];
+
+const COMPANY = [
+  {
+    display: "About Us",
+    url: "#",
+  },
+  {
+    display: "Career",
+    url: "#",
+  },
+  {
+    display: "Ranking",
+    url: "#",
+  },
+  {
+    display: "Contact Us",
+    url: "/contact",
+  },
+];
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="6" sm="6">
+            <div className="logo">
+              <h2 className="d-flex gap-2 align-items-center">
+                <span>
+                  <i className="ri-fire-fill" />
+                </span>
+                NFTs
+              </h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+                quos rerum voluptates debitis pariatur eveniet ab! Sed doloribus
+                mollitia hic?
+              </p>
+            </div>
+          </Col>
+          <Col lg="2" md="3" sm="6">
+            <h5>My Account</h5>
+            <ListGroup className="list__group">
+              {MY__ACOUNT.map((item, index) => (
+                <ListGroupItem key={index} className="list__item">
+                  <Link to={item.url}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="2" md="3" sm="6">
+            <h5>Resources</h5>
+            <ListGroup className="list__group">
+              {RESOURCES.map((item, index) => (
+                <ListGroupItem key={index} className="list__item">
+                  <Link to={item.url}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="2" md="3" sm="6">
+            <h5>Company</h5>
+            <ListGroup className="list__group">
+              {COMPANY.map((item, index) => (
+                <ListGroupItem key={index} className="list__item">
+                  <Link to={item.url}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="3" md="6" sm="6">
+            <h5>Newsletter</h5>
+            <input type="text" className="newsletter" placeholder="Email" />
+            <div className="social__links d-flex gap-3 align-items-center">
+              <span>
+                <Link to="#">
+                  <i className="ri-facebook-line"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="#">
+                  <i className="ri-instagram-line"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="#">
+                  <i className="ri-twitter-line"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="#">
+                  <i className="ri-telegram-line"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="#">
+                  <i className="ri-discord-line"></i>
+                </Link>
+              </span>
+            </div>
+          </Col>
+          <Col lg="12" className="mt-4 text-center">
+            <p className="copyright">
+              Copyrights 2022. Developed by Paul Tran. Rainstorm Studio
+              Australia All rights reserved.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
