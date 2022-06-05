921dcd54ba3aa64d3d62d006118cc26e04c6e838
Luan Tran <luan.t3@gmail.com>
2022-06-05 13:35:41 +0700

    create raw layout / home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import './app.css'
+import Layout from './components/Layout/Layout';
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <Layout />
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
@@ -7,7 +8,9 @@ import "remixicon/fonts/remixicon.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
