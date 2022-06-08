8dc3e1caf88109500f08eeefd5365696e9314703
Paul Tran <rsluan.github@gmail.com>
2022-06-08 19:32:51 +0700

    finished home page styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
 import "./header.css";
 import { Container } from "reactstrap";
 import { NavLink, Link } from "react-router-dom";
@@ -23,8 +23,31 @@ const NAV__LINKS = [
 ];
 
 const Header = () => {
+
+  const headerRef = useRef(null)
+
+  const menuRef = useRef(null)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add('header__shrink')
+      }
+      else {
+        headerRef.current.classList.remove('header__shrink')
+      }
+    })
+
+    return () => {
+      window.removeEventListener('scroll')
+    }
+  },[])
+
+
+  const toggleMenu = () => menuRef.current.classList.toggle('active__menu')
+
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <Container>
         <div className="navigation">
           <div className="logo">
@@ -36,7 +59,7 @@ const Header = () => {
             </h2>
           </div>
 
-          <div className="nav__menu">
+          <div className="nav__menu" ref={menuRef} onClick={toggleMenu}>
             <ul className="nav__list">
               {NAV__LINKS.map((item, index) => (
                 <li className="nav__item" key={index}>
@@ -62,7 +85,7 @@ const Header = () => {
             </button>
 
             <span className="mobile__menu">
-              <i className="ri-menu-line"></i>
+              <i className="ri-menu-line" onClick={toggleMenu}></i>
             </span>
           </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Container } from 'reactstrap'
+import HeroSection from '../components/ui/HeroSection'
+import LiveAuction from '../components/ui/Live-auction/LiveAuction'
+import SellerSection from '../components/ui/Seller-section/SellerSection'
+import StepSection from '../components/ui/Step-sesction/StepSection'
+import Trending from '../components/ui/Trending-section/Trending'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <HeroSection />
+      <LiveAuction />
+      <SellerSection />
+      <Trending />
+      <StepSection />
+    </>
   )
 }
 
